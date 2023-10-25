ed70f2a2dc688d29723d4b0f9dd7d34eadca34cd
David <davka8989@gmail.com>
2023-10-25 20:45:54 +0300

    added RPC support between logger and broker

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -7,7 +7,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 )
 
@@ -44,7 +46,10 @@ func main() {
 		Models: data.New(client),
 	}
 
-	//go app.serve()
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
+	log.Println("Starting server on port", webPort)
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
@@ -56,17 +61,22 @@ func main() {
 	}
 }
 
-//func (app *Config) serve() {
-//	srv := &http.Server{
-//		Addr:    fmt.Sprintf(":%s", webPort),
-//		Handler: app.routes(),
-//	}
-//
-//	err := srv.ListenAndServe()
-//	if err != nil {
-//		log.Panic()
-//	}
-//}
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+}
 
 func connectToMongo() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(mongoURL)
